3a5b730b584b47d60b8dde0ea41b1625bcf92396
eiki tsutou <eikitsutou0920@gmail.com>
2022-03-08 07:15:45 +0000

    add file

---
+++ kadai-ruby-2.rb
@@ -0,0 +1,20 @@
+def FizzBuzz(x)
+  if x % 3 == 0 && x % 5 == 0
+    return 'FizzBuzz'
+    
+  elsif x % 5 == 0
+    return 'Buzz'
+  
+  elsif x % 3 == 0
+    return 'Fizz'
+    
+  else
+    return x
+  end
+end
+  
+num_max = 100
+
+(1..num_max).each do |number|
+  puts FizzBuzz(number)
+end
